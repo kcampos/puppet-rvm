46a168cedafbc359a9f89f58461f4d2be54175a8
Kyle Campos <kyle.campos@gmail.com>
2012-11-07 15:22:28 -0800

    New: added gem_source.pp to manage gem repositories

---
+++ manifests/define/gem_source.pp
@@ -0,0 +1,38 @@
+# Manage gem repo sources
+define rvm::define::gem_source(
+  $ensure = 'present',
+  $ruby_version,
+) {  
+  ## Set sensible defaults for Exec resource
+  Exec {
+    path    => '/usr/local/rvm/bin:/bin:/sbin:/usr/bin:/usr/sbin',
+  }
+  
+  # Local Parameters
+  $rvm_path = '/usr/local/rvm'
+  $rvm_ruby = "${rvm_path}/rubies"
+  $rvm_depency = "install-ruby-${ruby_version}"      
+  
+  # Setup proper install/uninstall commands based on gem version.
+  $gem_source = {
+    'install'   => "rvm ${ruby_version}; gem sources -a ${name}",
+    'uninstall' => "rvm ${ruby_version}; gem sources -r ${name}",
+    'lookup'    => "rvm ${ruby_version}; gem sources -l | grep ${name}",
+  }
+
+  
+  ## Begin Logic
+  if $ensure == 'present' {
+    exec { "rvm-gem_source-install-${name}-${ruby_version}":
+      command => $gem['install'],
+      unless  => $gem['lookup'],
+      require => [Class['rvm'], Exec[$rvm_depency]],
+    }
+  } elsif $ensure == 'absent' {
+    exec { "rvm-gem_source-uninstall-${name}-${ruby_version}":
+      command => $gem['uninstall'],
+      onlyif  => $gem['lookup'],
+      require => [Class['rvm'], Exec[$rvm_depency]],
+    }    
+  }
+}
